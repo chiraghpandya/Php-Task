d6a38fb6ab968f6465a3cbba4ff01492be2214b2
chiraghpandya <pandyac815@gmail.com>
2025-08-22 11:05:55 +0530

    converting .html into .php

---
+++ Oberlo-1.0.0/header.php
@@ -0,0 +1,37 @@
+<!-- header section strats -->
+<header class="header_section">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg custom_nav-container ">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class=""> </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav  ">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""> Models </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""> About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link navbar-brand" href=""> <span><span>OB</span>erlo</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""> Price</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""> Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="">Contact </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+         </div>
+      </header>
